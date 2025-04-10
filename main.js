5425574f11e44ba4311302d4beb4c3018998b463
Vitória Cripa Viannini <vitoriacripa@gmail.com>
2025-04-10 00:23:31 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,17 @@
+let alisson_nota_av1 = 8;
+let alisson_nota_av2 = 9.6;
+let alisson_media = (alisson_nota_av1 + alisson_nota_av2) / 2 
+
+console.log ("Média Alisson:" + alisson_media);
+
+
+console.log("Exemplo com arrays")
+let notas_alisson = [8, 9.6];
+console.log(notas_alisson);
+
+console.log("AV1: "+notas_alisson[0]);
+console.log("AV2: "+notas_alisson[1]);
+notas_alisson[2] = (notas_alisson[0] + notas_alisson[1]) / 2;
+console.log("Média: " +notas_alisson[2]);
+console.log("Todas: " +notas_alisson);
+
